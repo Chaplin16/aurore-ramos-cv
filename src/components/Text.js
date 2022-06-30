031689edc6544ebde57740a447420f5ab8d7d72a
Chaplin16 <chaplin16@orange.fr>
2022-06-30 13:28:34 +0200

    retouche text.js

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,7 +1,7 @@
 export default function Text() {
   return (
     <div className='text text-align-left pt-3 pe-4 mb-5 w-100 content'>
-        <p>Passionée par le developpement Web, je travaille autant avec des clients qu'avec des developpeurs en sous-traitance. Intégration de maquettes souvent en boostrap et js, remise à jour sur base de données, création de sites vitrines, rédaction de contenu textuel visible, réalisation de vidéos avec montage son et image, création de maquettes sur Figma, saisie de données.. Grande conscience professionnelle, riguoureuse, travailleuse et toujours de bonne humeur, je respecte les demandes du client et ses délais.  Discrète, je ne publie en aucun cas le travail effectué en sous-traitance. Toujours en quête de nouvelles missions, de nouveaux défis, n'hésitez pas à me contacter par message, mail, visio, téléphone suivant votre préférence. </p>
+        <p>Passionée par le developpement Web, je travaille autant avec des clients qu'avec des developpeurs en sous-traitance. Intégration de maquettes souvent en boostrap et js, remise à jour de base de données, création de sites vitrines, rédaction de contenu textuel, réalisation de vidéos avec montage son et image, création de maquettes sur Figma, saisie de données.. Grande conscience professionnelle, riguoureuse, travailleuse et toujours de bonne humeur, je respecte les demandes du client et ses délais.  Discrète, je ne publie en aucun cas le travail effectué en sous-traitance. Toujours en quête de nouvelles missions, de nouveaux défis, n'hésitez pas à me contacter par message, mail, visio, téléphone suivant votre préférence. </p>
     </div>
   )
 }
